18d96b3d6ce22e450013d9e5df4b3f251bd1353d
jiakai.li <li.jiakai0305@gmail.com>
2020-08-19 21:56:41 +1200

    add genre class

--- domainmodel/genre.py
+++ domainmodel/genre.py
@@ -1,3 +1,27 @@
-
 class Genre:
-    pass
+
+    def __init__(self, genre_name: str):
+        if genre_name == "" or type(genre_name) is not str:
+            self.__genre_name = None
+        else:
+            self.__genre_name = genre_name.strip()
+
+    @property
+    def genre_name(self) -> str:
+        return self.__genre_name
+
+    def __repr__(self) -> str:
+        return f"<Genre {self.__genre_name}>"
+
+    def __eq__(self, other: 'Genre') -> bool:
+        if type(self) == type(other) and self.genre_name == other.genre_name:
+            return True
+        return False
+
+    def __lt__(self, other: 'Genre') -> bool:
+        if type(self) == type(other) and self.genre_name < other.genre_name:
+            return True
+        return False
+
+    def __hash__(self):
+        return hash(self.genre_name)
