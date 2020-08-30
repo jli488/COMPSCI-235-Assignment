fedbe62e47834c167afdd7b53f739af18ba86f77
jiakai.li <li.jiakai0305@gmail.com>
2020-08-30 13:58:24 +1200

    add Person class

--- domainmodel/actor.py
+++ domainmodel/actor.py
@@ -1,32 +1,14 @@
-class Actor:
+from domainmodel.person import Person
+
+
+class Actor(Person):
     def __init__(self, actor_full_name: str):
-        if actor_full_name == "" or type(actor_full_name) is not str:
-            self.__actor_full_name = None
-        else:
-            self.__actor_full_name = actor_full_name.strip()
+        super().__init__(actor_full_name)
         self.__colleague_set = set()
 
     @property
     def actor_full_name(self) -> str:
-        return self.__actor_full_name
-
-    def __repr__(self) -> str:
-        return f"<Actor {self.actor_full_name}>"
-
-    def __eq__(self, other: 'Actor') -> bool:
-        if type(self) == type(other) and self.actor_full_name == other.actor_full_name:
-            return True
-        return False
-
-    def __lt__(self, other: 'Actor') -> bool:
-        if type(self) == type(other) and self.actor_full_name < other.actor_full_name:
-            return True
-        if type(self) != type(other):
-            raise TypeError(f"Cannot compare Actor instance with {type(other)}")
-        return False
-
-    def __hash__(self):
-        return hash(self.actor_full_name)
+        return self._name
 
     def add_actor_colleague(self, colleague: 'Actor'):
         if type(self) != type(colleague):
--- domainmodel/director.py
+++ domainmodel/director.py
@@ -1,29 +1,7 @@
-class Director:
+from domainmodel.person import Person
 
-    def __init__(self, director_full_name: str):
-        if director_full_name == "" or type(director_full_name) is not str:
-            self.__director_full_name = None
-        else:
-            self.__director_full_name = director_full_name.strip()
 
+class Director(Person):
     @property
     def director_full_name(self) -> str:
-        return self.__director_full_name
-
-    def __repr__(self):
-        return f"<Director {self.director_full_name}>"
-
-    def __eq__(self, other: 'Director'):
-        if type(self) == type(other) and self.director_full_name == other.director_full_name:
-            return True
-        return False
-
-    def __lt__(self, other: 'Director'):
-        if type(self) == type(other) and self.director_full_name < other.director_full_name:
-            return True
-        if type(self) != type(other):
-            raise TypeError(f"Cannot compare Director instance with {type(other)}")
-        return False
-
-    def __hash__(self):
-        return hash(self.director_full_name)
+        return self._name
