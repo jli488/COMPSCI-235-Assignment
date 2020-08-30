dcb50b5c6165ff8559ebdf9de42f0a59debd4848
jiakai.li <li.jiakai0305@gmail.com>
2020-08-30 14:09:48 +1200

    tidy code

--- utils/movie_reader.py
+++ utils/movie_reader.py
@@ -52,13 +52,3 @@ class MovieFileCSVReader(object):
 
                 for director in self._read_field(record, 'Director', ','):
                     self._dataset_of_directors.add(Director(director))
-
-
-filename = r'D:\UoA\235_software_development_method\assignment\CS235FlixSkeleton\datafiles\Data1000Movies.csv'
-movie_file_reader = MovieFileCSVReader(filename)
-movie_file_reader.read_csv_file()
-
-print(f'number of unique movies: {len(movie_file_reader.dataset_of_movies)}')
-print(f'number of unique actors: {len(movie_file_reader.dataset_of_actors)}')
-print(f'number of unique directors: {len(movie_file_reader.dataset_of_directors)}')
-print(f'number of unique genres: {len(movie_file_reader.dataset_of_genres)}')
