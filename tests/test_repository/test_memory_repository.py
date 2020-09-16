41ad1b619160aa89ad0fd297437478c09f9506d2
jiakai-li <li.jiakai0305@gmail.com>
2020-09-16 12:11:51 +1200

    add memory repo test init

--- tests/test_repository/test_memory_repository.py
+++ tests/test_repository/test_memory_repository.py
@@ -1,5 +1,7 @@
-import pytest
+from movie.domainmodel.movie import Movie
 
 
 def test_repository_can_add_movie(memory_repo):
-    print(memory_repo.get_number_of_movies())
+    movie = Movie('Test Movie', 2020)
+    memory_repo.add_movie(movie)
+    assert memory_repo.get_movie('test movie', 2020) is movie
